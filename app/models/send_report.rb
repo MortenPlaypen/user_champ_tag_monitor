20e444d054bf7ff28cf2958634e0346333249284
Morten Lundsby Jensen <morten@playpenlabs.com>
2015-07-13 13:09:19 -0700

    parsing threads and formatted email

--- app/models/send_report.rb
+++ app/models/send_report.rb
@@ -25,6 +25,15 @@ class SendReport
 	def self.get_conversations(report)
 		auth = {:password => "X", :username => report.user.helpscout_token}
 		response = HTTParty.get(URI.encode("https://api.helpscout.net/v1/mailboxes/#{report.mailbox_hsid}/conversations.json?tag=#{report.tag}"), :basic_auth => auth)
-		response["items"].first["preview"]
+		ret_html = ""
+		response["items"].each do |item|
+			conversation_id = item["id"]
+			thread_response = HTTParty.get(URI.encode("https://api.helpscout.net/v1/conversations/#{conversation_id}.json"), :basic_auth => auth)
+			ret_html += "#{thread_response['item']['subject']}"
+			ret_html+= "</br>"
+			ret_html += "#{thread_response['item']['threads'].last['body']}"
+			ret_html += "</br>"
+		end
+		return ret_html
 	end
 end
