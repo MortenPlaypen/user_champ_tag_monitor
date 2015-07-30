575fb42a4f1c451679773ddc16e979c5f20cf6cd
Morten Lundsby Jensen <morten@playpenlabs.com>
2015-07-30 13:35:10 -0700

    added active status on report

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722202039) do
+ActiveRecord::Schema.define(version: 20150730201644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150722202039) do
     t.datetime "updated_at"
     t.string   "mailbox_hsid"
     t.string   "send_time"
+    t.boolean  "active"
   end
 
   create_table "users", force: true do |t|
